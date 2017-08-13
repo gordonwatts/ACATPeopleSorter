e2156305193df36849527a2b333671e2ce1d7f02
Gordon Watts <gordon@gordonandpaula.net>
2017-08-13 01:39:59 -0400

    First version of matching code

--- ACATListsLibrary/ListUtils.cs
+++ ACATListsLibrary/ListUtils.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ACATListsLibrary
@@ -31,10 +32,32 @@ namespace ACATListsLibrary
                 return Enumerable.Range(0, 10000)
                     .Select(i => p.Read())
                     .Where(i => i != null)
-                    .Select(i => new Presenters() { Name = i[0], Email = i[1], SpeakerInfo = i[4] })
+                    .Select(i => new Presenters() { Name = i[0], Email = i[1].AsUnifiedEmail(), SpeakerInfo = i[4] })
                     .ToArray();
             }
+        }
 
+        /// <summary>
+        /// Load indico registrations from registered.csv
+        /// </summary>
+        /// <remarks>
+        /// 1. Go to the registrations listing in indico
+        /// 2. Select "all"
+        /// 3. Click on expert to csv
+        /// 4. Save it to a file called registrations.csv
+        /// </remarks>
+        /// <returns></returns>
+        public static IndicoRegistration[] LoadIndicoRegistered()
+        {
+            using (var reader = File.OpenText("registrations.csv"))
+            {
+                var p = new CsvParser(reader);
+                return Enumerable.Range(0, 10000)
+                    .Select(i => p.Read())
+                    .Where(i => i != null)
+                    .Select(i => new IndicoRegistration() { Name = i[1], Email = i[3].AsUnifiedEmail()})
+                    .ToArray();
+            }
         }
 
         /// <summary>
@@ -78,6 +101,79 @@ namespace ACATListsLibrary
             string[] tokens = System.Text.RegularExpressions.Regex.Split(cvsString, pattern);
 
             return tokens;
+        }
+
+        public class IndicoRegistration
+        {
+            public string Name;
+            public string Email;
+        }
+
+        /// <summary>
+        /// Normalize the email address, and if there is an alias list, use it.
+        /// </summary>
+        /// <param name="origEmail"></param>
+        /// <returns></returns>
+        public static string AsUnifiedEmail(this string origEmail)
+        {
+            LoadEmailAliasList();
+
+            var t = origEmail.Trim();
+            return _email_aliases.ContainsKey(t)
+                ? _email_aliases[t]
+                : t;
+        }
+
+        /// <summary>
+        /// Remember a registered email so that we can make a mapping later on.
+        /// </summary>
+        /// <param name="aliasEmail"></param>
+        /// <param name="registeredEmail"></param>
+        public static void AddEmailAssociation(string aliasEmail, string registeredEmail)
+        {
+            LoadEmailAliasList();
+            _email_aliases[aliasEmail] = registeredEmail;
+            SaveEmailAliasList();
+        }
+
+        /// <summary>
+        /// Save the list of email aliases
+        /// </summary>
+        private static void SaveEmailAliasList()
+        {
+            var lines = _email_aliases
+                .Select(a => $"{a.Key} -> {a.Value}")
+                .ToArray();
+            File.WriteAllLines(_email_alias_filename, lines);
+        }
+
+        const string _email_alias_filename = "email_aliases.txt";
+
+        static Dictionary<string, string> _email_aliases = null;
+
+        /// <summary>
+        /// Load in the list of registered emails
+        /// </summary>
+        private static void LoadEmailAliasList()
+        {
+            if (_email_aliases == null)
+            {
+                _email_aliases = new Dictionary<string, string>();
+                if (File.Exists(_email_alias_filename))
+                {
+                    var associations = File.ReadAllLines(_email_alias_filename);
+                    var splitter = new Regex(@"^([^\s]+)\s*->\s*([^\s]+)$");
+                    foreach (var a in associations)
+                    {
+                        var m = splitter.Match(a);
+                        if (!m.Success)
+                        {
+                            throw new ArgumentException($"Unable to parse alias line {a}.");
+                        }
+                        _email_aliases[m.Groups[1].Value] = m.Groups[2].Value;
+                    }
+                }
+            }
         }
     }
 }
--- FindMissingPresenters/Program.cs
+++ FindMissingPresenters/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using static ACATListsLibrary.ListUtils;
+using static System.Console;
 
 namespace FindMissingPresenters
 {
@@ -16,7 +17,58 @@ namespace FindMissingPresenters
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            // SHould we prompt or just dump?
+            const bool ask_to_update_with_associations = true;
+
+            // Fetch the presenters and registered lists, and go from there.
             var presenters = LoadPresenters();
+            var registered = LoadIndicoRegistered();
+
+            var missing = from p in presenters
+                          where !registered.Any(r => r.Email == p.Email)
+                          select p;
+
+            Console.WriteLine($"There are {missing.Count()} presenters not yet registered");
+            foreach (var m in missing)
+            {
+                Console.WriteLine($"{m.Name}, {m.Email}");
+            }
+
+            // Next, lets see if we can match them by matching first and last names.
+            WriteLine();
+            WriteLine("Guesses as to who each of the missing registered folks might be");
+            var possibleMatches = from m in missing
+                                  from r in registered
+                                  where m.Name == r.Name
+                                  group r by m;
+
+            foreach (var missingGuesses in possibleMatches)
+            {
+                WriteLine($"{missingGuesses.Key.Name} - {missingGuesses.Key.Email} might be:");
+                foreach (var g in missingGuesses)
+                {
+                    WriteLine($"  {g.Name} - {g.Email}");
+
+                    if (ask_to_update_with_associations)
+                    {
+                        Write("  --> Update email assocation file? [y/n]: ");
+                        while (true)
+                        {
+                            var k = ReadKey();
+                            var goodK = k.KeyChar.ToString().ToLower();
+                            if (goodK == "y")
+                            {
+                                AddEmailAssociation(missingGuesses.Key.Email, g.Email);
+                                break;
+                            } else if (goodK == "n")
+                            {
+                                break;
+                            }
+                        }
+                        WriteLine();
+                    }
+                }
+            }
         }
     }
 }
